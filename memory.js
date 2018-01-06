0bd2e895f087e8b688273f4310cac418b9ccd3a2
Filpet96 <Fpetersson96@gmail.com>
2018-01-06 23:34:24 +0100

    create new element function

--- memory.js
+++ memory.js
@@ -2,3 +2,10 @@ start = function() {
     const starGame = function(selector) {
       return document.querySelector(selector);
     };
+    // Create an element with given parameters else creates a div
+    const newElem = function(elementName) {
+      const elem = document.createElement(
+        elementName ? elementName.toString() : "div"
+      );
+      return elem;
+    };
