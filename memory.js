8fab90d9475f74767c46a5dfd1c7a9d9e658dfee
Filpet96 <Fpetersson96@gmail.com>
2018-01-07 10:33:30 +0100

    add end tags

--- memory.js
+++ memory.js
@@ -1,40 +1,41 @@
 start = function() {
-    const starGame = function(selector) {
-      return document.querySelector(selector);
-    };
-    // Create an element with given parameters else creates a div
-    const newElem = function(elementName) {
-      const elem = document.createElement(
-        elementName ? elementName.toString() : "div"
-      );
-      return elem;
-    };
-    newElem.prototype.getactor = function() {
-      return this.elem.getAttribute("data-actor");
-    };
+  const starGame = function(selector) {
+    return document.querySelector(selector);
+  };
+  // Create an element with given parameters else creates a div
+  const newElem = function(elementName) {
+    const elem = document.createElement(
+      elementName ? elementName.toString() : "div"
+    );
+    return elem;
+  };
+  newElem.prototype.getactor = function() {
+    return this.elem.getAttribute("data-actor");
+  };
 
-    function shuffle(array) {
-      let currentIndex = array.length,
-        temporaryValue,
-        randomIndex;
-      // While there remain elements to shuffle...
-      while (0 !== currentIndex) {
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
+  function shuffle(array) {
+    let currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
 
-        // And swap it with the current element.
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-      }
-      return array;
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
     }
-    let busy = false;
-    let p_elem;
+    return array;
+  }
+  let busy = false;
+  let p_elem;
 
-    const Memory = function(level) {
-      this.level = level ? Number(level) : 1;
-      this.open_actor = -1;
-      this.matched_actors = [];
-    };
+  const Memory = function(level) {
+    this.level = level ? Number(level) : 1;
+    this.open_actor = -1;
+    this.matched_actors = [];
+  };
+};
