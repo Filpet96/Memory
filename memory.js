80cff4741eb2b419779601a7219458b67a9064ef
Filip <Fpetersson96@gmail.com>
2018-01-31 21:28:54 +0100

    updated messages and fixed bad inputs, also fixed small bug with restart game

--- memory.js
+++ memory.js
@@ -14,18 +14,18 @@ start = function() {
   };
 
   function shuffle(array) {
-    let currentIndex = array.length,
+    let currentIndexUsed = array.length,
       temporaryValue,
       randomIndex;
     // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
+    while (0 !== currentIndexUsed) {
+      // Picking a remaining eleement
+      randomIndex = Math.floor(Math.random() * currentIndexUsed);
+      currentIndexUsed -= 1;
 
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
+      // And im swapping it with the current used element.
+      temporaryValue = array[currentIndexUsed];
+      array[currentIndexUsed] = array[randomIndex];
       array[randomIndex] = temporaryValue;
     }
     return array;
